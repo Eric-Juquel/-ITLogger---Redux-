050ed1d8b8ace52066535f9cdc49d485aa0306f1
Eric J <ejuquel@yahoo.fr>
2020-04-17 18:51:40 +0200

    issue on store.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React, {Fragment, useEffect} from 'react';
+import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
-import AddTechModal from './components/techs/AddTechModal';
-import TechListModal from './components/techs/TechListModal';
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-import './App.css';
+import "./App.css";
 
-
-const  App = () => {
+const App = () => {
   useEffect(() => {
     // Init Materialize JS
-    M.AutoInit(); 
-  })
+    M.AutoInit();
+  });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
-}
+};
 
 export default App;
