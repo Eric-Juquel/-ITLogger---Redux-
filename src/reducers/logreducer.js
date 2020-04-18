5cd9feff8bbe631fdf2164f2ae280f7918e99f1c
Eric J <ejuquel@yahoo.fr>
2020-04-18 18:44:54 +0200

    Fixed

--- src/reducers/logreducer.js
+++ src/reducers/logreducer.js
@@ -50,7 +50,7 @@ export default (state = initialState, action) => {
     case UPDATE_LOG:
       return {
         ...state,
-        logs: state.log.map(log =>
+        logs: state.logs.map(log =>
           log.id === action.payload.id ? action.payload : log
         )
       };
